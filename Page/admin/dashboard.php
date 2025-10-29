96a01580dfd07fd22b9b223e1459744fd95062eb
Cika <cikazhrasy@gmail.com>
2025-10-29 18:00:04 +0700

    jgn dicoba

--- Page/admin/dashboard.php
+++ Page/admin/dashboard.php
@@ -1781,41 +1781,61 @@
     <!--  Main wrapper -->
     <div class="body-wrapper">
       <!--  Header Start -->
-      <header class="app-header">
-          <nav class="navbar">
-            <ul class="navbar-left">
-              <li class="nav-item">
-                <button class="sidebartoggler" id="headerCollapse">
-                  ☰
-                </button>
-              </li>
+       <header class="app-header">
+        <nav class="navbar navbar-expand-lg bg-dark">
+          <ul class="navbar-nav">
+            <li class="nav-item d-block d-xl-none">
+              <a class="nav-link sidebartoggler " id="headerCollapse" href="javascript:void(0)">
+                <i class="ti ti-menu-2"></i>
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link " href="javascript:void(0)" id="drop1" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="ti ti-bell"></i>
+                <div class="notification bg-primary rounded-circle"></div>
+              </a>
+              <div class="dropdown-menu dropdown-menu-animate-up" aria-labelledby="drop1">
+                <div class="message-body">
+                  <a href="javascript:void(0)" class="dropdown-item">
+                    Item 1
+                  </a>
+                  <a href="javascript:void(0)" class="dropdown-item">
+                    Item 2
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+          <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
+            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+               
               <li class="nav-item dropdown">
-                <a class="nav-link" href="#" id="notifBtn">
-                  🔔
-                  <div class="notification"></div>
+                <a class="nav-link " href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  <img src="./assets/images/profile/user-1.jpg" alt="" width="35" height="35" class="rounded-circle">
                 </a>
-                <div class="dropdown-menu" id="notifMenu">
-                  <a href="#" class="dropdown-item">Item 1</a>
-                  <a href="#" class="dropdown-item">Item 2</a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
+                  <div class="message-body">
+                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
+                      <i class="ti ti-user fs-6"></i>
+                      <p class="mb-0 fs-3">My Profile</p>
+                    </a>
+                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
+                      <i class="ti ti-mail fs-6"></i>
+                      <p class="mb-0 fs-3">My Account</p>
+                    </a>
+                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
+                      <i class="ti ti-list-check fs-6"></i>
+                      <p class="mb-0 fs-3">My Task</p>
+                    </a>
+                    <a href="./authentication-login.html" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
+                  </div>
                 </div>
               </li>
             </ul>
-
-            <div class="navbar-right">
-              <div class="nav-item dropdown">
-                <a class="nav-link" href="#" id="profileBtn">
-                  <img src="assets/images/profile/user-1.jpg" alt="User" width="35" height="35" class="profile-img">
-                </a>
-                <div class="dropdown-menu right" id="profileMenu">
-                  <a href="#" class="dropdown-item">My Profile</a>
-                  <a href="#" class="dropdown-item">My Account</a>
-                  <a href="#" class="dropdown-item">My Task</a>
-                  <a href="#" class="dropdown-item logout">Logout</a>
-                </div>
-              </div>
-            </div>
-          </nav>
-        </header>
+          </div>
+        </nav>
+      </header>
       </div>
     </div>
   </div>
