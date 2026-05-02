ef5a9a21c315a3be657f8c99bdcd1b97cddbfb9b
Cika Zahratus <cikazhrasy@gmail.com>
2026-05-02 19:54:45 +0700

    Refactor admin page structure by adding modular components for head, navbar, sidebar, and scripts

---
+++ Page/admin/admin-page/sidebar.php
@@ -0,0 +1,29 @@
+<aside class="left-sidebar">
+  <div>
+    <div class="brand-logo d-flex align-items-center justify-content-between">
+      <a href="../index.html" class="text-nowrap logo-img">
+        <img src="/Page/admin/assets/images/logos/logo.svg" alt="" />
+      </a>
+      <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+        <i class="ti ti-x fs-6"></i>
+      </div>
+    </div>
+    <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+      <ul id="sidebarnav">
+        <li class="nav-small-cap">
+          <iconify-icon icon="solar:menu-dots-linear" class="nav-small-cap-icon fs-4"></iconify-icon>
+          <span class="hide-menu">Home</span>
+        </li>
+        <li class="sidebar-item">
+          <a class="sidebar-link" href="../index.html"><i class="ti ti-atom"></i>Dashboard</a>
+        </li>
+        <li class="sidebar-item">
+          <a class="sidebar-link" href="input_artis.php"><i class="ti ti-aperture"></i>Artis</a>
+        </li>
+        <li class="sidebar-item">
+          <a class="sidebar-link" href="input_kategori.php"><i class="ti ti-aperture"></i>Kategori</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</aside>
